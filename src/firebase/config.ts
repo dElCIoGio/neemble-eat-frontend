5531fbe2a3770992c20517a53070b680170dce2e
Delcio <dagostinho4@gmail.com>
2025-06-05 20:20:18 +0100

    Debugging

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -13,6 +13,8 @@ const firebaseConfig = {
     measurementId: config.firebase.measurementId,
 }
 
+console.log(firebaseConfig)
+
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
