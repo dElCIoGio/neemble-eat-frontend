31c67eacfec35df1e0f10d3087026ddfd2d67359
Delcio Agostinho <64657972+dElCIoGio@users.noreply.github.com>
2025-07-08 14:30:06 +0100

    Show cart item count in restaurant menu navbar

--- src/components/pages/restaurant-menu/navbar.tsx
+++ src/components/pages/restaurant-menu/navbar.tsx
@@ -1,9 +1,14 @@
 import {Button} from "@/components/ui/button";
-import {Link} from "react-router";
-import {ShoppingCart} from "lucide-react"
+import {Link, useParams} from "react-router";
+import {ShoppingCart} from "lucide-react";
+import {useRestaurantMenuContext} from "@/context/restaurant-menu-context";
+import {useCart} from "@/hooks/use-cart";
 
 function Navbar() {
 
+    const {session, menu} = useRestaurantMenuContext()
+    const {restaurantSlug} = useParams() as unknown as {restaurantSlug: string, tableNumber: string}
+    const {numberOfItems} = useCart(restaurantSlug, session._id, menu._id)
 
     return (
         <div className={"flex space-x-4 justify-end p-4"}>
@@ -16,7 +21,13 @@ function Navbar() {
             <Button asChild size="sm" variant="secondary"
                     className={"text-zinc-600 hover:text-zinc-900"}>
                 <Link to={`cart`}
-                      className={"flex items-center space-x-2"}>
+                      className={"relative flex items-center space-x-2"}>
+                    {numberOfItems > 0 &&
+                        <span
+                            className="absolute -top-1 -right-1 text-[10px] bg-red-500 text-white rounded-full w-4 h-4 flex items-center justify-center">
+                            {numberOfItems}
+                        </span>
+                    }
                     <ShoppingCart/>
                     Carrinho
                 </Link>
